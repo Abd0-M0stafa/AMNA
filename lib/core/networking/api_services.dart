dada1fbcd6177f976e344b6b03b59e7e9b7bb01a
Ayman Moamed <darknaya.am@gmail.com>
2024-06-02 18:03:43 +0300

    some change

--- lib/core/networking/api_services.dart
+++ lib/core/networking/api_services.dart
@@ -2,17 +2,17 @@ import 'package:cancer/core/networking/api_constance.dart';
 import 'package:dio/dio.dart';
 
 class ApiServices {
-  final Dio dio;
+  final Dio _dio;
 
-  ApiServices(this.dio);
+  ApiServices(this._dio);
 
   Future<Map<String, dynamic>> get(
       {required String endPoint, Map<String, dynamic>? headers}) async {
-    final result = await dio.get(ApiConstance.baseUrl + endPoint,
+    final result = await _dio.get(ApiConstance.baseUrl + endPoint,
         options: Options(
             headers: headers ??
                 {
-                  'Content-Type': 'application/json',
+                  'Content-Type': ApiConstance.contentType,
                 }));
     return result.data;
   }
@@ -22,7 +22,7 @@ class ApiServices {
     Map<String, dynamic>? headers,
     required Map<String, dynamic> data,
   }) async {
-    final result = await dio.post(ApiConstance.baseUrl + endPoint,
+    final result = await _dio.post(ApiConstance.baseUrl + endPoint,
         data: data,
         options: Options(
             headers: headers ??
