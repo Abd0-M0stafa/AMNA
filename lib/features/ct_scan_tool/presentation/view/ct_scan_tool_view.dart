5a8d5a0b5397842c94057d8ca999a28de2fa2fef
AymanMohamed2 <darknaya.am@gmail.com>
2024-06-18 18:58:39 +0300

    ct scan tool body refactor

--- lib/features/ct_scan_tool/presentation/view/ct_scan_tool_view.dart
+++ lib/features/ct_scan_tool/presentation/view/ct_scan_tool_view.dart
@@ -1,9 +1,8 @@
 import 'package:cancer/core/utils/app_colors.dart';
 import 'package:cancer/core/utils/app_styles.dart';
-import 'package:cancer/features/ct_scan_tool/presentation/view/widgets/custom_button.dart';
-import 'package:cancer/features/ct_scan_tool/presentation/view/widgets/how_to_use.dart';
-import 'package:cancer/features/ct_scan_tool/presentation/view/widgets/upload_image.dart';
 import 'package:flutter/material.dart';
+
+import 'widgets/ct_scan_tool_view_body.dart';
 
 class CtScanToolView extends StatelessWidget {
   const CtScanToolView({super.key});
@@ -24,42 +23,7 @@ class CtScanToolView extends StatelessWidget {
         backgroundColor: AppColors.appBarColor,
       ),
       backgroundColor: AppColors.backColor,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: CustomScrollView(
-          slivers: [
-            const SliverToBoxAdapter(
-              child: SizedBox(
-                height: 40,
-              ),
-            ),
-            const SliverToBoxAdapter(
-              child: HowToUse(),
-            ),
-            const SliverToBoxAdapter(
-              child: SizedBox(
-                height: 30,
-              ),
-            ),
-            SliverToBoxAdapter(
-              child: UploadImage(),
-            ),
-            const SliverToBoxAdapter(
-              child: SizedBox(
-                height: 20,
-              ),
-            ),
-            SliverToBoxAdapter(
-              child: CustomButton(),
-            ),
-            const SliverToBoxAdapter(
-              child: SizedBox(
-                height: 30,
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: const CtScanToolViewBody(),
     );
   }
 }
