2a4ae273637f263b41d578ee60fab8c29d1620d4
Abd0-M0stafa <abdelrahman.azab688@gmail.com>
2024-06-18 17:47:27 +0300

    logos

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 abstract class AppColors {
   static const appBarColor = Color.fromARGB(255, 246, 243, 243);
   static const primary = Color(0xffc83869);
-  static const backColor = Color.fromARGB(255, 252, 228, 236);
+  static const backColor = Color(0xFFFCE4EC);
 }
