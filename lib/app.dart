12d14ace42ebad6907159b51048063baaf49d037
Abd0-M0stafa <abdelrahman.azab688@gmail.com>
2024-06-20 17:54:59 +0300

    updates

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,5 @@
 import 'package:cancer/features/splash/presentation/views/splash_view.dart';
-import 'package:cancer/features/welcome/presentation/view/wlcome_view.dart';
 import 'package:flutter/material.dart';
-
-import 'features/home/presentation/view/home_view.dart';
 
 class CancerApp extends StatelessWidget {
   const CancerApp({super.key});
